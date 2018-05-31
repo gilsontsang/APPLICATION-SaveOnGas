1875b80600da7ec4fc7734c032eb26dccc20a873
Gilson Tsang <37494996+gilsontsang@users.noreply.github.com>
2018-05-31 15:22:05 -0700

    Update SaveOnGas.js

--- SaveOnGas.js
+++ SaveOnGas.js
@@ -1,6 +1,7 @@
 $("#distKm").keypress(function(event) {
 	if(event.which === 13) {
   	distKm = parseFloat($(this).val());
+    $(this).removeAttr("placeholder");
     $(this).val("");
   }
 })
@@ -8,6 +9,7 @@ $("#distKm").keypress(function(event) {
 $("#litrePer100Km").keypress(function(event) {
 	if(event.which === 13) {
   	litrePer100Km = parseFloat($(this).val());
+    $(this).removeAttr("placeholder");
     $(this).val("");
   }
 })
@@ -15,6 +17,7 @@ $("#litrePer100Km").keypress(function(event) {
 $("#minsTravel").keypress(function(event) {
 	if(event.which === 13) {
   	minsTravel = parseFloat($(this).val());
+    $(this).removeAttr("placeholder");
     $(this).val("");
   }
 })
@@ -22,6 +25,7 @@ $("#minsTravel").keypress(function(event) {
 $("#cadPrice").keypress(function(event) {
 	if(event.which === 13) {
   	cadPrice = parseFloat($(this).val());
+    $(this).removeAttr("placeholder");
     $(this).val("");
   }
 })
@@ -29,6 +33,7 @@ $("#cadPrice").keypress(function(event) {
 $("#usdPrice").keypress(function(event) {
 	if(event.which === 13) {
   	usdPrice = parseFloat($(this).val());
+    $(this).removeAttr("placeholder");
     $(this).val("");
   }
 })
@@ -36,6 +41,7 @@ $("#usdPrice").keypress(function(event) {
 $("#litresFuel").keypress(function(event) {
 	if(event.which === 13) {
   	litresFuel = parseFloat($(this).val());
+    $(this).removeAttr("placeholder");
     $(this).val("");
   }
 })
@@ -44,16 +50,16 @@ $("#currencyCAD").keypress(function(event) {
 	if(event.which === 13) {
   	currencyCAD = parseFloat($(this).val());
     $(this).removeAttr("placeholder");
-    $(this).val("hello");
+    $(this).val("");
   }
 })
 
 $("#enter").on("click", function() {
   $("#distKmDisplay").text("Your round trip took " + distKm + " kilometers round trip.");
-  $("#litrePer100KmDisplay").text("Your average litres/100km is " + litrePer100Km + ".");
-  $("#minsTravelDisplay").text("It took you " + minsTravel + " to travel to the border and back.");
-  $("#cadPriceDisplay").text("Gas price around your neighbourhood is $" + cadPrice + " CDN.");
-  $("#usdPriceDisplay").text("Gas price at the borer is $" + usdPrice + " USD.");
+  $("#litrePer100KmDisplay").text("Your vehicle averages " + litrePer100Km + " litres per  100km.");
+  $("#minsTravelDisplay").text("It took you " + minsTravel + " minutes to travel to the border and back.");
+  $("#cadPriceDisplay").text("Gas price around your neighbourhood is $" + cadPrice + " CAD per litre.");
+  $("#usdPriceDisplay").text("Gas price at the borer is $" + usdPrice + " USD per gallon.");
   $("#litresFuelDisplay").text("You used " + litresFuel + " litres of fuel.");
-  $("#currencyCADDisplay").text("The currency at this time is " + currencyCAD + ".");
+  $("#currencyCADDisplay").text("The currency at this time is $" + currencyCAD + " CDN = $1.0 USD.");
 })
